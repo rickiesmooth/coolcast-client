f839553c4d718ebc7e7ce2730b991cfea22b9855
Rick <rick.smit@ahold.com>
2018-10-07 18:35:43 +0200

    correct structure

--- plugins/gatsby-plugin-apollo-client/gatsby-browser.js
+++ plugins/gatsby-plugin-apollo-client/gatsby-browser.js
@@ -1,6 +1,6 @@
 const React = require(`react`)
 const { ApolloProvider } = require('react-apollo')
-const { instance } = require('../../src/Client')
+const { instance } = require('../../src/components/Client')
 
 exports.wrapRootElement = ({ element }) => (
   <ApolloProvider client={instance}>{element}</ApolloProvider>
